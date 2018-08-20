3727b48a7c5b2c2463dbf361adfc86467cb86371
Lukas Mueller <mueller@malus.sgn.bti>
2018-08-20 09:59:32 -0400

    modify program to work with blastn version 2.2

--- bin/stitch.pl
+++ bin/stitch.pl
@@ -20,6 +20,7 @@ Lukas Mueller <lam87@cornell.edu>
 use strict;
 use Bio::SeqIO;
 use File::Slurp qw | read_file |;
+use Data::Dumper;
 
 my $seq_file = shift;
 my $match_file = shift;
@@ -55,42 +56,74 @@ if (-e $match_file.".parsed") {
 
 else { 
     open(my $F, "<", $match_file) || die "Can't open match file $match_file\n";
-    
-    
-    
+        
     $count = 0;
+    my $best_query;
+    my $best_subject;
+    my $best_score =0;
+    my $best_coord =0;
+    my $best_s_start = 0;
+    my $best_s_end = 0;
+    my $best_q_start = 0;
+    my $best_q_end = 0;
+    my $best_orientation;
+
     while (<$F>) { 
 	chomp;
-	my ($query, $subject, $percent, $aln_length, $a, $b, $q_start, $q_end, $s_start, $s_end, $evalue, $score) = split /\t/;
+	my ($query, $subject, undef,undef,undef, undef, undef,undef,undef,$percent, $qlen, $slen, $aln_length, $mismatch,$gapopen,$q_start, $q_end, $s_start, $s_end, $evalue, $score) = split /\t/;
+
+	#print STDERR "READING: $previous_query, $query, $subject, $s_start, $s_end, $score\n";
+	
 	
 	if ($previous_query ne $query) { 
-	    my $orientation = '+';
-	    if ($s_end < $s_start) { $orientation = '-'; }
 	    
-	    my $s_start_coord = 0;
-	    if ($orientation eq '+') { 
-		$s_start_coord = $s_start - $q_start;
-	    }
-	    else { 
-		$s_start_coord = $s_start - (length($seqs{$query}) - $q_end);
-	    }
-	    
-	    $subject=~s/lcl\|Contig(\d+).*/$1/g;
-	    push @matches, [ $query, $subject, $s_start_coord, $orientation, $q_start, $q_end, $s_start, $s_end, $percent, $aln_length, $evalue, $score ];
-	    $previous_query = $query;
+	    print STDERR "BEST: $best_query, $best_subject, $best_coord, $best_orientation, $best_score \n";
+
+	    if (!defined($query) ) { next; }
+	    push @matches, [ $best_query, $best_subject, $best_coord, $best_orientation, $best_score ];
+
+
+	    $best_score = 0;
+
 	}
 	if ($count % 1000000 == 0) { print STDERR "\rParsing blast report... ".int($count/1000000)."M lines ";}
 	$count++;
+	
+	if ($score > $best_score) { 
+	    $best_score = $score; 
+	    $best_query = $query;
+	    $best_subject = $subject;
+	    $best_s_start = $s_start;
+	    $best_s_end = $s_end;
+	    $best_q_start = $q_start;
+	    $best_q_end = $q_end;
+	    $best_orientation =  $s_end < $s_start ? '-' : '+';
+	    if ($best_orientation eq '+') { 
+		$best_coord = $best_s_start - $best_q_start;
+	    }
+	    else { 
+		$best_coord = $best_s_start - (length($seqs{$best_query}) - $best_q_end);
+	    }
+
+	}
+	$previous_query = $query;
+	
     }
+    push @matches, [ $best_query, $best_subject, $best_coord, $best_orientation, $best_score ];
 }
 
+
+
 print STDERR "Done.\n";
 
-print STDERR "Sorting matches... ";
-my @sorted_matches = sort { if ($a->[1] == $b->[1]) { $a->[2] <=> $b->[2] } else {  $a->[1] <=> $b->[1]  } } @matches;
+print STDERR Dumper(\@matches);
+#print STDERR "Sorting matches... ";
+my @sorted_matches = sort { if ($a->[1] eq $b->[1]) { $a->[2] <=> $b->[2] } else {  $a->[1] cmp $b->[1]  } } @matches;
 print STDERR "Done.\n";
 
-print STDERR "Write sorted assembly file... ";
+shift @sorted_matches; # remove first element, contains 0 values
+
+print STDERR "Write sorted assembly file... \n";
 
 if (!-e $match_file.".parsed") { 
     my $G;
@@ -108,14 +141,14 @@ my $open_file_handle =0;
 foreach my $line (@sorted_matches) { 
     my ($query, $subject, $start, $orientation) = @$line;
 
-    print STDERR "$subject, $previous_subject, $open_file_handle\n";    
+    print STDERR "SUBJECT: $subject, PREVIOUS_SUBJECZT:  $previous_subject, $open_file_handle\n";    
     if ($subject ne $previous_subject) { 
 
 	if ($open_file_handle) { close($H); }
 	open($H, ">", "stitched_chr$subject.fa") || die "Can't open chr file\n";
 	$open_file_handle=1;
-	print $H ">seq$subject\n";
-	print "\n>seq$subject\n";
+	#print $H ">seq$subject\n";
+	#print "\n>seq$subject\n";
     }
 
     my $seq = $seqs{$query};
@@ -133,8 +166,8 @@ foreach my $line (@sorted_matches) {
 
     if (! $open_file_handle) { die "ouch!\n"; }
 
-    print $seq;
-    print $n_seq;
+    #print $seq;
+    #print $n_seq;
     print $H $seq;
     print $H $n_seq;
     
